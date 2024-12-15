94ac03d3f3061a746c99d1f5bd559f394e0a6aab
Renato Monteiro <45536168+monteiro-renato@users.noreply.github.com>
2024-12-15 21:28:01 +0100

    operator-sdk create api --group k6 --version v1alpha1 --kind TestRun --resource --controller

--- cmd/main.go
+++ cmd/main.go
@@ -158,6 +158,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PrivateLoadZone")
 		os.Exit(1)
 	}
+	if err = (&controller.TestRunReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TestRun")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
